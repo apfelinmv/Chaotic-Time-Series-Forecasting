aaec6a90e3fdbb09a6c9e2187ff88bbcdf18ac58
apfelinmv <133810326+apfelinmv@users.noreply.github.com>
2025-05-26 02:11:04 +0300

    Update main2.py

--- Pseudo-Classifier/main2.py
+++ Pseudo-Classifier/main2.py
@@ -2,15 +2,12 @@ import matplotlib.pyplot as plt
 from data_generator import generate_lorenz_x
 from combined_runner import run_combined_prediction
 
-# 1) Генерируем ряд
 x = generate_lorenz_x()
 print("Длина ряда:", len(x))
 
-# 2) Параметры обучения и теста
 n_train = 6267
 n_test  = 50
 
-# 3) Запуск комбинированного прогноза (plot=False, чтобы рисовать самим)
 actual, preds, labels, mse = run_combined_prediction(
     series=x,
     n_train=n_train,
@@ -21,5 +18,3 @@ actual, preds, labels, mse = run_combined_prediction(
     threshold=0.1,
     plot=True
 )
-
-#500 норм
